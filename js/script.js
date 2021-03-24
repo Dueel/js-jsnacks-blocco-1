84a7cb58b0016c53b85c3a4589cba4101c978a23
Dueel <diego.bar@live.it>
2021-03-24 17:25:07 +0100

    Esercizio finito

---
+++ js/script.js
@@ -0,0 +1,48 @@
+var objStudent = {
+
+    'name': 'Giovanni',
+    'last name': 'Verga',
+    'age': 82
+}
+
+for (var key in objStudent){
+    document.getElementById('student').innerHTML += key + ': ' + objStudent[key] + '<br>'
+}
+
+var objStd1 = {
+
+    'name': 'Giacomo',
+    'last name': 'Leopardi'
+}
+
+var objStd2 = {
+
+    'name': 'Italo',
+    'last name': 'Calvino'
+}
+
+var objStd3 = {
+
+    'name': 'Luigi',
+    'last name': 'Pirandello'
+}
+
+var objStd4 = {
+
+    'name': prompt('Inserisci il nome'),
+    'last name': prompt('Inserisci il cognome'),
+    'age': prompt("Inserisci l'etá")
+}
+
+var arr = [objStd1, objStd2, objStd3, objStd4];
+
+console.log(arr);
+
+for (var i = 0; i < arr.length; i++){
+
+    for (var key in arr[i]){
+        console.log(key + ': ' + arr[i][key]);
+        document.getElementById('test').innerHTML += '<div>' + key + ': ' + arr[i][key] + '</div>';
+
+    }
+}
